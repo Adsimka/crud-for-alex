2a555c7043a1ee04d5088d277232709d9fb57e3b
Arsen <sahalysyk02@mail.ru>
2025-04-27 22:32:02 +0500

    fix

--- src/main/java/com/product_service/model/dto/ProductCreateDto.java
+++ src/main/java/com/product_service/model/dto/ProductCreateDto.java
@@ -8,7 +8,7 @@ public record ProductCreateDto(
         @Size(max = 20, message = "{product.name.size}")
         @NotBlank(message = "{product.name.black}")
         String name,
-        @Size(min = 10, max = 50, message = "{product.name.size}")
+        @Size(min = 10, max = 50, message = "{product.description.size}")
         String description,
         @Valid
         DetailCreateDto detail
