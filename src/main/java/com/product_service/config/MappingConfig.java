e8c9dcfd8636ceef7fd57f81fda9e3015c2f47c5
Arsen <sahalysyk02@mail.ru>
2025-05-02 02:59:32 +0500

    fix mapper config

--- src/main/java/com/product_service/config/MappingConfig.java
+++ src/main/java/com/product_service/config/MappingConfig.java
@@ -2,12 +2,14 @@ package com.product_service.config;
 
 import org.mapstruct.MapperConfig;
 import org.mapstruct.MappingConstants;
+import org.mapstruct.NullValueCheckStrategy;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
 
 @MapperConfig(
         componentModel = MappingConstants.ComponentModel.SPRING,
-        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.SET_TO_NULL,
+        nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS,
         unmappedTargetPolicy = ReportingPolicy.IGNORE
 )
 public interface MappingConfig {
